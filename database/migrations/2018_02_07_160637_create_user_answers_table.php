9741bc024247839d756aafe2c674dbe01225d109
Weje Emmanuel <e.weje@concept-nova.com>
2018-02-07 17:14:17 +0100

    Added migration files

--- database/migrations/2018_02_07_160637_create_user_answers_table.php
+++ database/migrations/2018_02_07_160637_create_user_answers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUserAnswersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_answers', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('image_path');
-            $table->rememberToken();
+            $table->integer('user_id')->unsigned();
+            $table->integer('quiz_option_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('quiz_option_id')->references('id')->on('quiz_options');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_answers');
     }
 }
